9bb5086d2e4cd6cd188cd778219471139629a961
tsavaph <tsavaph@gmail.com>
2023-10-10 20:14:36 +0300

    init auth service deleted mvn

---
+++ cargo-transportation-auth-service/src/main/java/ru/tsavaph/cargotransportationauthservice/CargoTransportationAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.tsavaph.cargotransportationauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoTransportationAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CargoTransportationAuthServiceApplication.class, args);
+    }
+
+}
---
+++ cargo-transportation-auth-service/src/test/java/ru/tsavaph/cargotransportationauthservice/CargoTransportationAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tsavaph.cargotransportationauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoTransportationAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
